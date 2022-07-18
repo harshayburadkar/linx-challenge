ef1de350893277b102b63af6482cc3cfe97514b3
HB <harshay.imag3@gmail.com>
2022-07-19 01:15:04 +0530

    feat: Target and Route selection endpoints
    Squashed commit of the following:
    
    commit 23e96397b8a7233a5096d64ab785edc5c2b05942
    Author: HB <harshay.imag3@gmail.com>
    Date:   Tue Jul 19 00:03:01 2022 +0530
    
        add route selection endpoints, logic and tests
    
    commit 596110ba364daa4744e23fe0f8dced5b7e94c5f5
    Author: HB <harshay.imag3@gmail.com>
    Date:   Mon Jul 18 21:54:22 2022 +0530
    
        implement target and targets endpoints
    
    commit d71ce3d45bd5a7811775c6d95d645f87aba7182d
    Author: HB <harshay.imag3@gmail.com>
    Date:   Sat Jul 16 01:22:36 2022 +0530
    
        fix issue with the tests
    
    commit 50d71f40682d5fbe83c07540a0bb685b8a17af71
    Author: HB <harshay.imag3@gmail.com>
    Date:   Fri Jul 15 04:10:08 2022 +0530
    
        add targets handlers with placeholder code

--- test/endpoints.js
+++ test/endpoints.js
@@ -4,14 +4,197 @@ var test = require('ava')
 var servertest = require('servertest')
 
 var server = require('../lib/server')
+var db = require('../lib/db')
+var { TARGETS_KEY } = require('../lib/constants')
+
+var sampleTarget = {
+  id: '1',
+  url: 'http://example.com',
+  value: '0.50',
+  maxAcceptsPerDay: '10',
+  accept: {
+    geoState: {
+      $in: ['ca', 'ny']
+    },
+    hour: {
+      $in: ['13', '14', '15']
+    }
+  }
+}
+
+test.beforeEach.cb(function (t) {
+  db.removeAllItems(TARGETS_KEY, function (err) {
+    if (err) console.log('Error initializing tests')
+    t.end()
+  })
+})
 
 test.serial.cb('healthcheck', function (t) {
   var url = '/health'
   servertest(server(), url, { encoding: 'json' }, function (err, res) {
     t.falsy(err, 'no error')
-
     t.is(res.statusCode, 200, 'correct statusCode')
     t.is(res.body.status, 'OK', 'status is ok')
     t.end()
+  })
+})
+
+test.serial.cb('get targets when targets is empty', function (t) {
+  var url = '/targets'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    const targets = res.body
+    t.falsy(err, 'no error')
+    t.deepEqual(targets.length, 0, 'response should be empty')
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.end()
+  })
+})
+
+test.serial.cb('get targets with targets is non empty', function (t) {
+  db.pushItemToList(TARGETS_KEY, sampleTarget, function (err) {
+    if (err) console.log('Error adding sample item')
+    var url = '/targets'
+    servertest(server(), url, { encoding: 'json' }, function (err, res) {
+      const targets = res.body
+      t.falsy(err, 'no error')
+      t.deepEqual(targets.length, 1, 'response should have one item')
+      t.is(res.statusCode, 200, 'correct statusCode')
+      t.end()
+    })
+  })
+})
+
+test.serial.cb('post targets', function (t) {
+  var url = '/targets'
+  var req = servertest(server(), url, { method: 'POST' }, function (err, res) {
+    t.falsy(err, 'no error')
+    db.getAllItemsFromList(TARGETS_KEY, function (err, targets) {
+      t.falsy(err, 'no error')
+      t.deepEqual(targets.length, 1, 'response should have one item')
+      t.is(res.statusCode, 200, 'correct statusCode')
+      t.end()
+    })
+  })
+  req.write(JSON.stringify(sampleTarget))
+  req.end()
+})
+
+test.serial.cb('get a particular target', function (t) {
+  db.pushItemToList(TARGETS_KEY, sampleTarget, function (err) {
+    if (err) console.log('Error adding sample item')
+    var url = '/target/1'
+    servertest(server(), url, { encoding: 'json' }, function (err, res) {
+      const target = res.body
+      t.falsy(err, 'no error')
+      t.truthy(target, 'some response is present')
+      t.deepEqual(sampleTarget.id, target?.id, 'item returned should be correct')
+      t.is(res.statusCode, 200, 'correct statusCode')
+      t.end()
+    })
+  })
+})
+
+test.serial.cb('not exisiting object should return not found', function (t) {
+  db.pushItemToList(TARGETS_KEY, sampleTarget, function (err) {
+    if (err) console.log('Error adding sample item')
+    var url = '/target/101'
+    servertest(server(), url, { encoding: 'json' }, function (err, res) {
+      t.falsy(err, 'no error')
+      t.is(res.statusCode, 404, 'correct statusCode')
+      t.end()
+    })
+  })
+})
+
+test.serial.cb('modify target with a specific id', function (t) {
+  db.pushItemToList(TARGETS_KEY, sampleTarget, function (err) {
+    if (err) console.log('Error adding sample item')
+    const id = '1'
+    var url = `/target/${id}`
+    var req = servertest(server(), url, { method: 'POST' }, function (err, res) {
+      t.falsy(err, 'no error')
+      db.getAllItemsFromList(TARGETS_KEY, function (err, targets) {
+        const editedItem = targets.find(i => i.id === id)
+        t.falsy(err, 'no error')
+        t.deepEqual(editedItem.value, 0.01, 'response should have edited value')
+        t.is(res.statusCode, 200, 'correct statusCode')
+        t.end()
+      })
+    })
+    const editedSampleTarget = { ...sampleTarget }
+    editedSampleTarget.value = 0.01
+    req.write(JSON.stringify(editedSampleTarget))
+    req.end()
+  })
+})
+
+test.serial.cb('test route with no match case', function (t) {
+  db.pushItemToList(TARGETS_KEY, sampleTarget, function (err) {
+    if (err) console.log('Error adding sample item')
+    var url = '/route'
+    var params = {
+      geoState: 'ca',
+      publisher: 'abc',
+      timestamp: '2018-07-19T23:28:59.513Z'
+    }
+    var req = servertest(server(), url, { method: 'POST', encoding: 'json' }, function (err, res) {
+      t.falsy(err, 'no error')
+      t.is(res.body?.decision, 'reject', 'should get reject as result')
+      t.end()
+    })
+    req.write(JSON.stringify(params))
+    req.end()
+  })
+})
+
+test.serial.cb('test route with simple match case', function (t) {
+  db.pushItemToList(TARGETS_KEY, sampleTarget, function (err) {
+    if (err) console.log('Error adding sample item')
+    var url = '/route'
+    var params = {
+      geoState: 'ca',
+      publisher: 'abc',
+      timestamp: '2018-07-19T13:28:59.513Z'
+    }
+    var req = servertest(server(), url, { method: 'POST', encoding: 'json' }, function (err, res) {
+      t.falsy(err, 'no error')
+      t.falsy(res.body?.decision === 'reject', 'should NOT get reject as result')
+      t.end()
+    })
+    req.write(JSON.stringify(params))
+    req.end()
+  })
+})
+
+test.serial.cb('test route with multiple match', function (t) {
+  db.pushItemToList(TARGETS_KEY, sampleTarget, function (err) {
+    if (err) console.log('Error adding sample item')
+    const highValueUrl = 'http://highvalue.com'
+    const highValueId = 2
+    const copySample = Object.assign({}, sampleTarget, { id: highValueId, value: '0.75', url: highValueUrl })
+    db.pushItemToList(TARGETS_KEY, copySample, function (err) {
+      if (err) console.log('Error adding sample item')
+      var url = '/route'
+      var params = {
+        geoState: 'ca',
+        publisher: 'abc',
+        timestamp: '2018-07-19T13:28:59.513Z'
+      }
+      var req = servertest(server(), url, { method: 'POST', encoding: 'json' }, function (err, res) {
+        t.falsy(err, 'no error')
+        const decision = res.body?.decision
+        t.deepEqual(highValueUrl, decision, 'result with higher value should be chosen')
+        db.getAllItemsFromList(TARGETS_KEY, function (err, targets) {
+          if (err) console.log('Error getting all items')
+          const item = targets.find(t => t.id === highValueId)
+          const expected = copySample.maxAcceptsPerDay - 1
+          const actual = item.maxAcceptsPerDay
+          t.deepEqual(actual, expected, 'accepts should reduce by one')
+          t.end()
+        })
+      })
+      req.write(JSON.stringify(params))
+      req.end()
+    })
   })
 })
